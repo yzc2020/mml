392533864e6776297b17df5e85f07afafd6c82d3
Yuzhc <53286102+yzc2020@users.noreply.github.com>
2023-12-06 23:18:59 +0800

    upload train and predict files

---
+++ ChemNet/config.py
@@ -0,0 +1,13 @@
+    args.init_lr = 5e-5
+    args.max_lr = 6e-4
+    args.final_lr = 4e-5
+    args.warmup_epochs = 2.0
+    args.num_lrs = 1
+    hidden_dim=512
+    fp2dim=512
+    fclayer  1
+    self_attentipn dropout=0
+    fpn_dropout=0
+    batch_size=32
+    epoch=30
+
